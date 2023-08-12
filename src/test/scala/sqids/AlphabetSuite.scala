003f3e6a95e3a823104deaef8046c17798e48891
Jesper Öman <jesper.oman@magine.com>
2023-08-12 16:53:44 +0200

    Alphabet shuffle tests

--- src/test/scala/sqids/AlphabetSuite.scala
+++ src/test/scala/sqids/AlphabetSuite.scala
@@ -4,32 +4,37 @@ import munit.ScalaCheckSuite
 
 class AlphabetSuite extends ScalaCheckSuite {
   test("simple") {
-    val sqids = Sqids.forAlphabet(
-      Alphabet("0123456789abcdef").toOption.get
-    )
-
     val numbers = List(1, 2, 3)
     val id = "4d9fd2"
-
-    assertEquals(sqids.encode(numbers), id)
-    assertEquals(sqids.decode(id), numbers)
+    Alphabet("0123456789abcdef")
+      .map(Sqids.forAlphabet)
+      .foreach { sqids =>
+        assertEquals(sqids.encode(numbers), id)
+        assertEquals(sqids.decode(id), numbers)
+      }
   }
 
-  test("short") {
-    val sqids = Sqids.forAlphabet(Alphabet("abcde").toOption.get)
-
+  test("short alphabet") {
     val numbers = List(1, 2, 3)
-    assertEquals(sqids.decode(sqids.encode(numbers)), numbers)
+    Alphabet("abcde")
+      .map(Sqids.forAlphabet)
+      .foreach(sqids => assertEquals(sqids.decode(sqids.encode(numbers)), numbers))
   }
 
-  test("long") {
-    val sqids = Sqids.forAlphabet(
-      Alphabet(
-        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_+|{}[];:'\"/?.>,<`~"
-      ).toOption.get
+  test("long alphabet") {
+    val numbers = List(1, 2, 3)
+    Alphabet(
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_+|{}[];:'\"/?.>,<`~"
     )
-    val numbers = List(1, 2, 3)
-    assertEquals(sqids.decode(sqids.encode(numbers)), numbers)
+      .map(Sqids.forAlphabet)
+      .foreach(sqids => assertEquals(sqids.decode(sqids.encode(numbers)), numbers))
   }
 
+  test("repeating alphabet characters") {
+    assert(Alphabet("aabcdefg").isLeft)
+  }
+
+  test("too short of an alphabet") {
+    assert(Alphabet("abcd").isLeft)
+  }
 }
